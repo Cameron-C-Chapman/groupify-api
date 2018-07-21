8a39bbb296e09be2745f6a36edbad79a5dd1c1d8
Cameron Chapman <Cameron.C.Chapman@gmail.com>
2018-07-21 18:19:47 -0500

    Adding POST user route and updating README docs.

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const SpotifyWebApi = require('spotify-web-api-node');
 const requestUtils = require('../utils/requestUtils');
+const { User } = require('../persist');
 
 /**
  * Get the user profile for the given user.
@@ -46,6 +47,39 @@ router.get('/:id', (req, res) => {
     .catch((error) => {
         res.status(error.statusCode).send(error.message);
     });
+});
+
+/**
+ * Create a user.
+ * 
+ * POST body params:
+ *      username
+ *      display_name
+ *      email
+ *      spotify_url
+ *      spotify_uri
+ *      spotify_id
+ * 
+ * @returns {Object}    Created user object.
+ */
+router.post('/', (req, res) => {
+    let newUser = {
+        username: req.body.username,
+        display_name: req.body.displayName,
+        email: req.body.email,
+        spotify_url: req.body.spotifyUrl,
+        spotify_uri: req.body.spotifyUri,
+        spotify_id: req.body.spotifyId
+    };
+
+    User.create(newUser)
+        .then((user) => {
+            res.status(201).send(user);
+        })
+        .catch(() => {
+            console.log('POST /user = ', { error: error, requestBody: req.body });
+            res.status(500).status('oh schnap, stuff got weird');
+        });
 });
 
 module.exports = router;
