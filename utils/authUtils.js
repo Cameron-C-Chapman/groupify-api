5166c2e796edb6850e24f962faa30c6a88300f19
Ivan Greene <ivan@ivan.sh>
2018-07-22 08:23:54 -0500

    Check for spotify id header

--- utils/authUtils.js
+++ utils/authUtils.js
@@ -4,7 +4,7 @@ const persist = require('../persist');
 const writeAuth = (req, res, next) => {
   let access_token = req.get('Authorization');
   let spotify_id = req.get('X-Spotify-ID');
-  if (access_token) {
+  if (access_token && spotify_id) {
     access_token = getAccessTokenFromHeader(access_token);
     persist.User.updateAuth({
       spotify_id, access_token,
